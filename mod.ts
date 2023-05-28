efb6558b568d3bda7b62c1dc540dcc73fc3c4ed0
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2023-05-28 10:41:26 +0900

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,5 +1,13 @@
 import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.38/deno-dom-wasm.ts";
 
-export function urlChange(html: string, change: (string) => URL | string){
-  
+function oneChange(document, query: string, attr: string, change: (string) => string): void{
+  for(const element of document.querySelectorAll(query)){
+    const raw = element.getAttribute(attr)
+    element.setAttribute(attr,change(raw))
+  }
+}
+export function urlChange(html: string, change: (string) => string){
+  const document = new DOMParser().parseFromString(html, "text/html")
+  oneChange(document, "a[href]", "href", change)
+  return document.querySelector("html").outerHTML
 }
